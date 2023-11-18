9f250d60a8b6fb3ba86a694e78dce5fccc21f604
Salman Abbas <salmanabbas985@gmail.com>
2023-11-18 18:33:02 +0500

    Server Changes

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -41,17 +41,37 @@ class ProfileController extends Controller
             ->where('user_role', 'customer')->first();
 
             if ($user) {
-                $address = new CustomerAddress();
-
-                $address->user_id = $user->id;
-                $address->first_name = $request->first_name;
-                $address->last_name = $request->last_name;
-                $address->street_address = $request->street_address;
-                $address->house = $request->house;
-                $address->country = $request->country;
-                $address->state = $request->state;
-                $address->zip_code = $request->zip_code;
-                $address->save();
+                
+                $check = CustomerAddress::where('user_id', $user->id)->first();
+                
+                if($check)
+                {
+                    $check->first_name = $request->first_name;
+                    $check->last_name = $request->last_name;
+                    $check->street_address = $request->street_address;
+                    $check->house = $request->house;
+                    $check->country = $request->country;
+                    $check->state = $request->state;
+                    $check->zip_code = $request->zip_code;
+                    $check->save();
+                }
+                else{
+                    
+                    $address = new CustomerAddress();
+    
+                    $address->user_id = $user->id;
+                    $address->first_name = $request->first_name;
+                    $address->last_name = $request->last_name;
+                    $address->street_address = $request->street_address;
+                    $address->house = $request->house;
+                    $address->country = $request->country;
+                    $address->state = $request->state;
+                    $address->zip_code = $request->zip_code;
+                    $address->save();
+                
+                    
+                }
+                    
 
 
                 $user = User::where('id', $user->id)
@@ -78,6 +98,7 @@ class ProfileController extends Controller
     public function changePassword(Request $request)
     {
         $validator = Validator::make($request->all(), [
+            'current_password' => 'required',
             'password' => 'required',
         ]);
 
@@ -98,16 +119,26 @@ class ProfileController extends Controller
 
             if ($user) {
 
-                $user->password = Hash::make($request->password);
-                $user->save();
-
-                $user = User::where('id', $user->id)
-                    ->with('customer_addresses')->first();
-
-                return response()->json([
-                    'status' => true,
-                    'data' => $user
-                ]);
+                if(Hash::check($request->current_password, $user->password))
+                {
+                    $user->password = Hash::make($request->password);
+                    $user->save();
+    
+                    $user = User::where('id', $user->id)
+                        ->with('customer_addresses')->first();
+    
+                    return response()->json([
+                        'status' => true,
+                        'data' => $user
+                    ]);
+                    
+                }
+                else{
+                    return response()->json([
+                        'status' => false,
+                        'msg' => 'Password does not matched!!'
+                    ]);
+                }
             } else {
                 return response()->json([
                     'status' => false,
@@ -145,6 +176,7 @@ class ProfileController extends Controller
                 ->where('user_role', 'customer')->first();
 
             if ($user) {
+
 
                 $user->name = isset($request->name) ? $request->name : $user->name;
                 $user->dob = isset($request->dob) ? $request->dob : $user->dob;
